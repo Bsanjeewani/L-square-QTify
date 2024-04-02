7dff1d66c5614f19c1ec8e546887d4bef50c4b2c
onkar <onkarvasav@gmail.com>
2024-04-02 23:52:32 +0530

    navbar

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>hello</h1>
-      </header>
+      <Navbar />
     </div>
   );
 }

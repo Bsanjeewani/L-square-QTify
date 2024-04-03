acc70f35c07364389c15751d74c2299fcfae9765
sanjeewani <sanjeewanibhoite@gmail.com>
2024-04-04 00:31:43 +0530

    new

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import HeroSection from "./components/HeroPage/HeroSection";
+import MainPage from "./components/MainPage/MainPage";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <HeroSection />
+      <MainPage />
     </div>
   );
 }
--- qtify/src/components/HeroPage/HeroSection.jsx
+++ qtify/src/components/HeroPage/HeroSection.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import styles from "./HeroSection.module.css";
 import Headphone from "../../assets/headphone.png";
 
-const HeroSection = () => {
+const HeroSection = ({ content, content2 }) => {
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.heroImageWrapper}>
           <div className={styles.banner}>
-            <h1>100 Thousand Songs, ad fee</h1>
-            <h1>Over thhousands podcast episodes</h1>
+            <h1>{content}</h1>
+            <h1>{content2}</h1>
           </div>
           <div>
             <img src={Headphone} alt="Headphone" height="212px" />

1d50ba3d7063b8763ef1b47ab97e49b05a595a37
sanjeewani <sanjeewanibhoite@gmail.com>
2024-04-04 01:02:48 +0530

    hero

--- qtify/src/components/HeroPage/HeroSection.jsx
+++ qtify/src/components/HeroPage/HeroSection.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import styles from "./HeroSection.module.css";
-import Headphone from "../../assets/headphone.png";
+import HeroLogo from "../HeroLogo";
 
 const HeroSection = ({ content, content2 }) => {
   return (
     <>
-      <div className={styles.wrapper}>
-        <div className={styles.heroImageWrapper}>
-          <div className={styles.banner}>
-            <h1>{content}</h1>
-            <h1>Over thhousands podcast episodes</h1>
-          </div>
-          <div>
-            <img src={Headphone} alt="Headphone" height="212px" />
-          </div>
+      <div className={styles.heroimage}>
+        <div>
+          <h1>100 Thousand Songs, ad-free</h1>
+          <h1>Over thousands podcast episodes</h1>
+        </div>
+
+        <div className={styles.herologo}>
+          <HeroLogo />
         </div>
       </div>
     </>

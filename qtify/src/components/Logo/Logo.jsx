77b58be1ed42901564577f4e3f571599b995b1ef
sanjeewani <sanjeewanibhoite@gmail.com>
2024-04-03 23:21:26 +0530

    changes

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Logo = () => {
-  return <img src={require("../../assets/logo.png")} alt="logo" width={67} />;
+  return (
+    <img
+      className="logo"
+      src={require("../../assets/logo.png")}
+      alt="logo"
+      width={67}
+    />
+  );
 };
 
 export default Logo;

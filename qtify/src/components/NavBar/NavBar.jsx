4f4b2993280b1fb725c0ccce6deb2757fd973b06
sanjeewani <sanjeewanibhoite@gmail.com>
2024-04-04 23:52:30 +0530

    Update NavBar.jsx

--- qtify/src/components/NavBar/NavBar.jsx
+++ qtify/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import "./Navbar.css"
-import Searchbar from '../Searchbar/Searchbar'
-import Feedback from '../FeedbackButton/FeedbackButton'
-import Logo from '../../Images/Logo'
+import React from "react";
+import "./Navbar.css";
+import Logo from "../../Images/Logo";
+import Search from "../Searchbar/Searchbar";
+import FeedbackForm from "../FeedbackForm/FeedbackForm";
 
 const Navbar = ({ onFeedbackButtonClick }) => {
-    return (
-        <div className='navbar'>
-            <div className='logo'><Logo/></div>
-            <div>
-                <Searchbar />
-            </div>
-            <div className='feedback'><Feedback onFeedbackButtonClick={onFeedbackButtonClick}/></div>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <Logo />
+      </div>
+      <div>
+        <Search />
+      </div>
+      <div className="feedback">
+        <FeedbackForm onFeedbackButtonClick={onFeedbackButtonClick} />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
